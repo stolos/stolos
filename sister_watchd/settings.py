bc22f965d98685c951248069469c6d54478c5284
Antonis Kalipetis <akalipetis@gmail.com>
2016-06-17 20:14:24 +0300

    Switch to DATABASE_URL for db config
    Add needed dependencies for PostgreSQL

--- sister_watchd/settings.py
+++ sister_watchd/settings.py
@@ -77,12 +77,11 @@ WSGI_APPLICATION = 'sister_watchd.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.path.join('sqlite:///', BASE_DIR, 'db.sqlite3'))
 }
 
 
