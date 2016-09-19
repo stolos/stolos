b1bf5505a8504778d835b4e0444eeecae6ba062b
Antonis Kalipetis <akalipetis@gmail.com>
2016-09-19 21:56:06 +0300

    Add key validation for public keys

--- users/models.py
+++ users/models.py
@@ -4,7 +4,21 @@ import uuid
 
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 from sshpubkeys import SSHKey
+from sshpubkeys.exceptions import MalformedDataException
+
+
+def _validate_public_key(value):
+    """
+    Validates that the given key is a valid SSH public key.
+    """
+    ssh_key = SSHKey(value, parse_options=False, strict_mode=True)
+    try:
+        ssh_key.parse()
+    except MalformedDataException as exc:
+        raise ValidationError(exc.message, code='invalid-key')
 
 
 class SSHPublicKey(models.Model):
@@ -14,7 +28,8 @@ class SSHPublicKey(models.Model):
     """
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4,
                             editable=False)
-    public_key = models.TextField(unique=True)
+    public_key = models.TextField(unique=True,
+                                  validators=[_validate_public_key])
     md5 = models.CharField(unique=True, max_length=256, editable=False)
     sha256 = models.CharField(unique=True, max_length=256, editable=False)
     sha512 = models.CharField(unique=True, max_length=256, editable=False)
