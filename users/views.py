41119a181c93b78735af593ef420e6937ad7c1b5
TDaglis <thanasis@sourcelair.com>
2016-10-27 16:20:46 +0300

    Add custom Login View to create docker certs on login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import requests
+from django.utils import crypto
+from djoser import utils
+from djoser.views import LoginView
+from users.models import DockerCert
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    """CustomLoginView to create docker certificates bound to the new token.
+    """
+
+    def _action(self, serializer):
+        token = utils.login_user(self.request, serializer.user)
+        token_serializer_class = serializers.serializers_manager.get('token')
+        alphabet = string.ascii_lowercase + string.digits
+        random_cn = crypto.get_random_string(32, alphabet)
+        cfssl_url = 'test'  # TODO: update to cfssl server endpoint
+        certs = requests.post(cfssl_url, random_cn) # TODO: fix request
+        token = token_serializer_class(token).data['auth_token']
+        DockerCert.create(token=token, cert_cn=random_cn)
+
+        response_data = {
+            'auth_token': token,
+            'docker_key_pem': certs['docker_key_pem'],
+            'docker_cert_pem': certs['docker_cert_pem'],
+            'docker_ca_pem': certs['docker_ca_pem']
+        }
+        return Response(
+            data=response_data,
+            status=status.HTTP_200_OK,
+        )
