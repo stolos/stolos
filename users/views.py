0f5bdc785f80fcebbfd93fe8bbc7aee79e670da8
TDaglis <thanasis@sourcelair.com>
2016-11-01 15:25:13 +0200

    Fix cfssl request for new certs

--- users/views.py
+++ users/views.py
@@ -1,38 +1,48 @@
+import string
+
 import requests
 from django.utils import crypto
 from djoser import utils
-from djoser.views import LoginView
+from djoser import serializers
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework import status
+from rest_framework.response import Response
+
 from users.models import DockerCert
 
-class CustomLoginView(LoginView):
+class CustomLoginView(utils.ActionViewMixin, generics.GenericAPIView):
     """CustomLoginView to create docker certificates bound to the new token.
     """
+    serializer_class = serializers.serializers_manager.get('login')
+    permission_classes = (
+        permissions.AllowAny,
+    )
 
-    def _action(self, serializer):
+    def action(self, serializer):
         token = utils.login_user(self.request, serializer.user)
         token_serializer_class = serializers.serializers_manager.get('token')
         alphabet = string.ascii_lowercase + string.digits
         random_cn = crypto.get_random_string(32, alphabet)
         payload = {
-            "CN": random_cn,
+            "CN": str(random_cn),
             "hosts": [""],
             "names": {
                 "C": "GR",
                 "O": "SourceLair PC",
                 "ST": "Athens"
             },
-            "profile": random_cn
+            "profile": 'client'
         }
         response = requests.post(
-            'cfssl/api/v1/cfssl/newcert', payload
+            'http://cfssl:8888/api/v1/cfssl/newcert', json=payload
         )
-        token = token_serializer_class(token).data['auth_token']
         DockerCert.objects.create(token=token, cert_cn=random_cn)
 
         response_data = {
-            'auth_token': token,
-            'docker_key_pem': response['result']['private_key'],
-            'docker_cert_pem': response['result']['certificate']
+            'auth_token': token_serializer_class(token).data['auth_token'],
+            'docker_key_pem': response.json()['result']['private_key'],
+            'docker_cert_pem': response.json()['result']['certificate']
         }
         return Response(
             data=response_data,
