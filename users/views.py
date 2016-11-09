6d7f263835037c9dfa100cb036e16c9c2c66d7a3
Antonis Kalipetis <akalipetis@gmail.com>
2016-11-09 12:16:44 +0200

    Handle cases when a certificate can't be created
    Also, simplify code.

--- users/views.py
+++ users/views.py
@@ -9,7 +9,9 @@ from rest_framework import permissions
 from rest_framework import status
 from rest_framework.response import Response
 
+from users.exceptions import CFSSLError
 from users.models import DockerCert
+
 
 class CustomLoginView(utils.ActionViewMixin, generics.GenericAPIView):
     """CustomLoginView to create docker certificates bound to the new token.
@@ -19,11 +21,8 @@ class CustomLoginView(utils.ActionViewMixin, generics.GenericAPIView):
         permissions.AllowAny,
     )
 
-    def action(self, serializer):
-        token = utils.login_user(self.request, serializer.user)
-        token_serializer_class = serializers.serializers_manager.get('token')
-        alphabet = string.ascii_lowercase + string.digits
-        random_cn = crypto.get_random_string(32, alphabet)
+    def _create_docker_cert(self, token):
+        random_cn = crypto.get_random_string(32)
         payload = {
             'request': {
                 'CN': random_cn,
@@ -42,18 +41,25 @@ class CustomLoginView(utils.ActionViewMixin, generics.GenericAPIView):
             },
             'profile': 'client',
         }
-        response = requests.post(
-            'http://cfssl:8888/api/v1/cfssl/newcert', json=payload
-        )
+        try:
+            response = requests.post(
+                'http://cfssl:8888/api/v1/cfssl/newcert', json=payload
+            )
+            response.raise_for_status()
+        except requests.exceptions.RequestException as err:
+            raise CFSSLError(err.message)
         DockerCert.objects.create(
-            token=token, cert_cn=random_cn, owner=serializer.user)
+            token=token, cert_cn=random_cn, owner=token.user)
+        result = response.json()['result']
+        return result['certificate'], result['private_key']
 
+    def action(self, serializer):
+        token = utils.login_user(self.request, serializer.user)
+        token_serializer_class = serializers.serializers_manager.get('token')
+        certificate, private_key = self._create_docker_cert(token)
         response_data = {
             'auth_token': token_serializer_class(token).data['auth_token'],
-            'docker_key_pem': response.json()['result']['private_key'],
-            'docker_cert_pem': response.json()['result']['certificate']
+            'docker_key_pem': private_key,
+            'docker_cert_pem': certificate,
         }
-        return Response(
-            data=response_data,
-            status=status.HTTP_200_OK,
-        )
+        return Response(data=response_data, status=status.HTTP_200_OK)
